d1b4f45fdae84699a7688be29578e2ffec2de428
H-Ding-Xue <hdingxue@gmail.com>
2021-12-10 05:40:28 +0800

    code and comment pages

--- FYP_code/main.py
+++ FYP_code/main.py
@@ -9,5 +9,17 @@ def home():
 def comment():
     return render_template("comments.html")
 
+@app.route("/code generation")
+def code():
+    return render_template("codes.html")
+
+@app.route("/voicebot")
+def voicebot():
+    return render_template("voicebot.html")
+
+@app.route("/invalid_input")
+def invalid_input():
+    return render_template("invalid_input.html")
+    
 if __name__ == "__main__":
     app.run(debug = True)
