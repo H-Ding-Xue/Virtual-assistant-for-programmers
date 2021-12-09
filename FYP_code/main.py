099e7131bc0ec4b587905cdb436d98f7e72f8949
chuahx <83070674+chuahx@users.noreply.github.com>
2021-12-09 22:45:01 +0800

    Update main.py
    add route for voice assistant and invalid input

--- FYP_code/main.py
+++ FYP_code/main.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/voicebot")
+def voicebot():
+    return render_template("voicebot.html")
+
+@app.route("/invalid_input")
+def invalid_input():
+    return render_template("invalid_input.html")
+    
 if __name__ == "__main__":
     app.run()
