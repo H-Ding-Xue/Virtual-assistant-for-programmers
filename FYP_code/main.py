e8f527a06c22d08d8f607f77a98b67006e17566d
H-Ding-Xue <hdingxue@gmail.com>
2021-12-23 15:27:10 +0800

    finished implementation of code generation from comments

--- FYP_code/main.py
+++ FYP_code/main.py
@@ -1,4 +1,5 @@
-from flask import Flask,redirect,url_for,render_template
+from flask import Flask,redirect,url_for,render_template, request
+import pickle
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,9 +10,18 @@ def home():
 def comment():
     return render_template("comments.html")
 
-@app.route("/code generation")
+@app.route("/code generation", methods=["POST","GET"])
 def code():
-    return render_template("codes.html")
+    if request.method == "POST":
+        loaded_vectorizer = pickle.load(open('saved_codegen_vectorizer', 'rb'))
+        loaded_model = pickle.load(open('saved_codegen_model', 'rb'))
+        codedesc = request.form["pseudoinput"]
+        predicted_codeblock = loaded_model.predict(loaded_vectorizer.transform([codedesc]))
+        predicted_codeblock = predicted_codeblock[0]
+        predicted_codeblock = predicted_codeblock.replace(r'\n', '\n')
+        return render_template("codes.html",predicted_codeblock=predicted_codeblock)
+    else:
+        return render_template("codes.html")
 
 @app.route("/code generation(EIEO)")
 def codeEIEO():
