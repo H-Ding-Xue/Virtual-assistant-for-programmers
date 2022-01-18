360635013fbbe25d16f89a5657230541c42ec947
chuahx <83070674+chuahx@users.noreply.github.com>
2022-01-18 18:35:34 +0800

    move common js to base.js

--- FYP_code/static/js/codes.js
+++ FYP_code/static/js/codes.js
@@ -1,6 +1,4 @@
 $( document ).ready(function() {
-    // set alerts to display for only 3s if any
-    setInterval(function(){ $(".alert").fadeOut(); }, 3000);
     $('#recbutton').click(function(){
         //timeout function with delay else disable click will not work
         setTimeout(function() {
--- FYP_code/static/js/invalid_input.js
+++
@@ -1,4 +0,0 @@
-$( document ).ready(function() {
-    // set alerts to display for only 3s if any
-    setInterval(function(){ $(".alert").fadeOut(); }, 3000);
-})
