44ebaa922ca6106be305c13d83d4ff53c1ac928d
chuahx <83070674+chuahx@users.noreply.github.com>
2022-01-30 23:21:20 +0800

    removed voice input js and css

--- FYP_code/static/js/codes.js
+++ FYP_code/static/js/codes.js
@@ -1,13 +1,4 @@
 $( document ).ready(function() {
-    $('#recbutton').click(function(){ 
-        //timeout function with delay else disable click will not work
-        setTimeout(function() {
-            // disable click almost instantly
-            $('#recbutton').prop('disabled', true);
-          }, 1); 
-          //animation effect
-        $('#recbutton').addClass("Rec");  
-    });
     $("textarea").keydown(function(e) {
         if(e.keyCode === 9) { 
             var start = this.selectionStart;
