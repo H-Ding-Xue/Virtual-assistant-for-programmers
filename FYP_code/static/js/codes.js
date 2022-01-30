5f26ebf52521c38284838c9c23dcf56a8e6ba48f
H-Ding-Xue <hdingxue@gmail.com>
2022-01-30 23:14:21 +0800

    added usage of tab to make spaces for code generation

--- FYP_code/static/js/codes.js
+++ FYP_code/static/js/codes.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    $('#recbutton').click(function(){
+    $('#recbutton').click(function(){ 
         //timeout function with delay else disable click will not work
         setTimeout(function() {
             // disable click almost instantly
@@ -7,5 +7,21 @@ $( document ).ready(function() {
           }, 1); 
           //animation effect
         $('#recbutton').addClass("Rec");  
+    });
+    $("textarea").keydown(function(e) {
+        if(e.keyCode === 9) { 
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+    
+            var $this = $(this);
+    
+            $this.val($this.val().substring(0, start)
+                        + "    "  
+                        + $this.val().substring(end));
+    
+            this.selectionStart = this.selectionEnd = start + 4;
+    
+            return false;
+        }
     });
 })
